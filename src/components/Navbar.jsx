119df4e97562f394298091c29050d464ec3a44a5
Farid Amimri <farid.amimri@gmail.com>
2022-12-12 10:21:56 +0100

    react navbar menu

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+/** @format */
+
+import React from 'react'
+import styled from 'styled-components'
+import Badge from '@mui/material/Badge'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import LanguageIcon from '@mui/icons-material/Language'
+import SettingsIcon from '@mui/icons-material/Settings'
+import Avatar from '@mui/material/Avatar'
+
+function Navbar() {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Logo>BiBi.</Logo>
+        </Left>
+        <Right>
+          <Icons>
+            <Badge badgeContent={4} color='error'>
+              <NotificationsNoneIcon />
+            </Badge>
+            <Badge badgeContent={2} color='error'>
+              <LanguageIcon />
+            </Badge>
+            <SettingsIcon />
+            <Avatar sx={{ width: 24, height: 24, bgcolor: 'green' }} />
+          </Icons>
+        </Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
+
+const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: white;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Left = styled.div``
+
+const Right = styled.div`
+  width: 140px;
+`
+
+const Logo = styled.span`
+  font-weight: bold;
+  font-size: 30px;
+  color: darkblue;
+  cursor: pointer;
+`
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: #555;
+`
