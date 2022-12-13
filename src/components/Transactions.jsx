dddefa4ada32a0fb79e48170f67213dc87625837
Farid Amimri <farid.amimri@gmail.com>
2022-12-13 21:45:32 +0100

    Create Transactions component to render latest transactions

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,134 @@
+/** @format */
+
+import React from 'react'
+import styled from 'styled-components'
+import TableContainer from '@mui/material/TableContainer'
+import Table from '@mui/material/Table'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import Paper from '@mui/material/Paper'
+import Avatar from '@mui/material/Avatar'
+import MuiButton from './MuiButton'
+
+function Transactions() {
+  return (
+    <Container>
+      <Title>Latest transactions</Title>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='transactions table'>
+          <TableHead>
+            <TableRow>
+              <TableCell className='tableHead'>Customer</TableCell>
+              <TableCell className='tableHead'>Date</TableCell>
+              <TableCell className='tableHead'>Amount</TableCell>
+              <TableCell className='tableHead'>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                <Profil>
+                  <Avatar
+                    src='https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                    sx={{ width: 30, height: 30 }}
+                  />
+                  <Name>Susan Carol</Name>
+                </Profil>
+              </TableCell>
+              <TableCell>2 jun 2022</TableCell>
+              <TableCell>122.00 €</TableCell>
+              <TableCell>
+                <MuiButton color='green' type='Approved' />
+              </TableCell>
+            </TableRow>
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                <Profil>
+                  <Avatar
+                    src='https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                    sx={{ width: 30, height: 30 }}
+                  />
+                  <Name>Susan Carol</Name>
+                </Profil>
+              </TableCell>
+              <TableCell>2 jun 2022</TableCell>
+              <TableCell>122.00 €</TableCell>
+              <TableCell>
+                <MuiButton color='error' type='Declined' />
+              </TableCell>
+            </TableRow>
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                <Profil>
+                  <Avatar
+                    src='https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                    sx={{ width: 30, height: 30 }}
+                  />
+                  <Name>Susan Carol</Name>
+                </Profil>
+              </TableCell>
+              <TableCell>2 jun 2022</TableCell>
+              <TableCell>122.00 €</TableCell>
+              <TableCell>
+                <MuiButton color='green' type='Approved' />
+              </TableCell>
+            </TableRow>
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component='th' scope='row'>
+                <Profil>
+                  <Avatar
+                    src='https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                    sx={{ width: 30, height: 30 }}
+                  />
+                  <Name>Susan Carol</Name>
+                </Profil>
+              </TableCell>
+              <TableCell>2 jun 2022</TableCell>
+              <TableCell>122.00 €</TableCell>
+              <TableCell>
+                <MuiButton color='warning' type='Pending' />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  )
+}
+
+export default Transactions
+
+const Container = styled.div`
+  flex: 2;
+  padding: 20px;
+  box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+  .tableHead {
+    font-weight: bold;
+  }
+`
+
+const Title = styled.h3`
+  font-size: 22px;
+  font-weight: 600;
+  margin-bottom: 10px;
+`
+
+const Profil = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Name = styled.span`
+  padding-left: 10px;
+  font-weight: 600;
+`
