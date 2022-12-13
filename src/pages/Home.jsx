7ffac4cc41e33757f37f7a8714991e07aac2e55a
Farid Amimri <farid.amimri@gmail.com>
2022-12-13 17:20:04 +0100

    Add Chart component & users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,26 @@
 
 import React from 'react'
 import styled from 'styled-components'
+import { userData } from '../data'
 import Features from '../components/Features'
+import Chart from '../components/Chart'
+import Users from '../components/Users'
+import Transactions from '../components/Transactions'
 
 function Home() {
   return (
     <Container>
       <Features />
+      <Chart
+        data={userData}
+        title='User Analytics'
+        dataKey='Active User'
+        grid
+      />
+      <Widgets>
+        <Users />
+        <Transactions />
+      </Widgets>
     </Container>
   )
 }
@@ -16,4 +30,9 @@ export default Home
 
 const Container = styled.div`
   flex: 4;
+`
+
+const Widgets = styled.div`
+  display: flex;
+  margin: 20px 0 20px 20px;
 `
